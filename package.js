ff4096d5d831eb553fb2b9d7a77a4b6c0d9fead5
Andrew F <id@andrewfong.com>
2014-12-23 15:15:15 -0800

    Increment version number

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'fongandrew:find-and-modify',
   summary: 'findAndModify implementation for Meteor collection',
-  version: '0.1.0',
+  version: '0.1.1',
   git: 'https://github.com/fongandrew/meteor-find-and-modify.git'
 });
 
