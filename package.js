76964444060321d8b2cefcf3d7661ad4adc1adf5
fongandrew <lx2798@gmail.com>
2016-02-22 01:20:04 -0800

    Add missing packages for Meteor 1.2, fixes #13

--- package.js
+++ package.js
@@ -1,13 +1,15 @@
 Package.describe({
   name: 'fongandrew:find-and-modify',
   summary: 'findAndModify implementation for Meteor collection',
-  version: '0.2.1',
+  version: '0.2.2',
   git: 'https://github.com/fongandrew/meteor-find-and-modify.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0.4');
+  api.use('ddp', ['client', 'server']);
   api.use('mongo', ['client', 'server']);
+  api.use('random', ['client', 'server']);
   api.imply('mongo', ['client', 'server'])
   api.addFiles('find_and_modify.js', ['client', 'server']);
 });
