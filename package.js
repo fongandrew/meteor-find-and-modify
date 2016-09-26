b44a0470455096269672f0870ca707526b436f1d
Vagrant user <vagrant@packer-virtualbox-iso.vm>
2016-09-26 02:09:38 +0000

    Version bump

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
   name: 'fongandrew:find-and-modify',
   summary: 'findAndModify implementation for Meteor collection',
-  version: '0.2.2',
+  version: '1.0.0',
   git: 'https://github.com/fongandrew/meteor-find-and-modify.git'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.4');
+  api.versionsFrom('1.4.1.1');
   api.use('ddp', ['client', 'server']);
   api.use('mongo', ['client', 'server']);
   api.use('random', ['client', 'server']);
@@ -17,5 +17,5 @@ Package.onUse(function(api) {
 Package.onTest(function(api) {
   api.use('tinytest');
   api.use('fongandrew:find-and-modify');
-  api.addFiles('find_and_modify_tests.js');
+  api.mainModule('find_and_modify_tests.js');
 });
